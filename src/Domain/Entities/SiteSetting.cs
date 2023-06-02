331d17b21e499635eaab12493f51f9f91b02dbb3
SEYHMUS <SEYHMUS@ARSLAN>
2023-06-02 10:14:31 +0300

    Tema genel olarak ayarlandı

---
+++ src/Domain/Entities/SiteSetting.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class SiteSetting:BaseEntity
+    {
+        public int SiteSettingId { get; set; }
+        public string SystemName { get; set; }
+        public string SiteLogoPath { get; set; }
+
+    }
+}
---
+++ src/WebUI/Pages/JobPosting.cshtml.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebUI.Pages
+{
+    public class JobPostingModel:PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
